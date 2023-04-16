898fb09b802fc33ed31890007969acce2f3482f9
Nureke <Snurekeee03@gmail.com>
2023-04-16 15:00:34 +0600

    Initial Commit

---
+++ sajdaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sajdaApp
+//
+//  Created by Nurzhan Saktaganov on 16.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
